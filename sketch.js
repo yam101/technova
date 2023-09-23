b3c1ee6e85e879277fd6d6780a3dce557b514d93
Simra Khan <simrakhan7605@gmail.com>
2023-09-23 14:31:47 -0400

    added intro scene text

--- sketch.js
+++ sketch.js
@@ -5,6 +5,10 @@ function preload() {
   font = loadFont('fonts/Abaddon Bold.ttf');
 
   mainText[0] = "I’m so nervous for my first day of work!";
+  mainText[1] = "It’s a new company, a new boss, and a new position, too; I’m a developer!";
+  mainText[2] = "I have been coding during my own free time for a few years now, but I have never done it professionally…";
+  mainText[3] = "I applied for a job at this tech company because my friend told me to, and I guess they were impressed enough by my projects, because I’m here now…";
+  mainText[4] = "I hope everything goes well…";
 }
 
 function setup() {
