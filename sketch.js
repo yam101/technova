475f4fccddd56fd9d98e192a166c97cc24c81604
Simra Khan <simrakhan7605@gmail.com>
2023-09-23 15:23:10 -0400

    added background comments

--- sketch.js
+++ sketch.js
@@ -12,12 +12,13 @@ function preload() {
   mike = []
 
   // intro monologue scene
+  // BACKGROUND: office lounge
   mainText[0] = "I’m so nervous for my first day of work!";
   mainText[1] = "It’s a new company, a new boss, and a new position, too; I’m a developer!";
   mainText[2] = "I have been coding during my own free time for a few years now, but I have never done it professionally...";
   mainText[3] = "I applied for a job at this tech company because my friend told me to, and I guess they were impressed enough by my projects, because I’m here now...";
   mainText[4] = "I hope everything goes well...";
-  // scene changes to office
+  // BACKGROUND: boss' office
   mainText[5] = "Oh my god, thats my new boss over there!";
   mainText[6] = "I should introduce myself to him.";
   // conversation with boss begins
@@ -35,7 +36,8 @@ function preload() {
   mainText[18] = "Okay! Thank you!";
   mainText[19] = "Hm, that was a bit disappointing, I was really excited to utilize my programming skills… I have a lot of experience in web dev and I was hoping I could apply them.";
   mainText[20] = "Well hopefully I’ll get to do that on the next project...";
-  // conversation with colleague begins 
+  // conversation with Mike begins 
+  // BACKGROUND: Mike's place
   mainText[21] = "Hey, could you get me a coffee?";
   mainText[22] = "...what?";
   mainText[23] = "Listen, I’m super stressed with this big tech project, so some coffee would really hit the spot right now. Okay?";
@@ -50,13 +52,14 @@ function preload() {
   mainText[32] = "...Can you still grab me that coffee, though?";
   mainText[33] = "Umm... okay... sure...";
   // breakdown monologue begins
+  // BACKGROUND: all black
   mainText[34] = "I was so excited to work on the project...";
   mainText[35] = "Maybe I should just forget about all of this...";
   mainText[36] = "Applying to this job was such a stupid idea anyways, why did I ever think I could become some big-shot developer?";
   mainText[37] = "I shouldn’t have even bothered trying...";
   mainText[38] = "I’m really not cut out for this, am I?";
   mainText[39] = "...What is even the point of this...";
-
+  // PART B BEGINS
 }
 
 function setup() {
