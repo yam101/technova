768e7bc30d310084288e98c6717bdc3255a36677
Simra Khan <simrakhan7605@gmail.com>
2023-09-23 15:04:17 -0400

    added all main dialogue

--- sketch.js
+++ sketch.js
@@ -16,41 +16,41 @@ function preload() {
   mainText[5] = "Oh my god, thats my new boss over there!";
   mainText[6] = "I should introduce myself to him.";
   // conversation with boss begins
-  mainText[4] = "Hi, I'm the new employee, and its my first day here! Where can I start?";
-  mainText[4] = "Oh, hey. Welcome to the team.";
-  mainText[4] = "We are working on a pretty big project right now, so it would be a little hard to squeeze you in...";
-  mainText[4] = "I guess for now you can help the team document the project...";
-  mainText[4] = "Oh... I thought I was going to be working on the development team?";
-  mainText[4] = "Hm, well, you’re a little new for that, aren’t you? ";
-  mainText[4] = "Don’t want you to end up all confused.";
-  mainText[4] = "Maybe we can revisit that later.";
-  mainText[4] = "The job description said they needed a new developer because they needed help with the project...";
-  mainText[4] = "Oh, okay. I understand...";
-  mainText[4] = "So, you should head on over to Mike now. He’ll show you where to go.";
-  mainText[4] = "Okay! Thank you!";
-  mainText[4] = "Hm, that was a bit disappointing, I was really excited to utilize my programming skills… I have a lot of experience in web dev and I was hoping I could apply them.";
-  mainText[4] = "Well hopefully I’ll get to do that on the next project...";
+  mainText[7] = "Hi, I'm the new employee, and its my first day here! Where can I start?";
+  mainText[8] = "Oh, hey. Welcome to the team.";
+  mainText[9] = "We are working on a pretty big project right now, so it would be a little hard to squeeze you in...";
+  mainText[10] = "I guess for now you can help the team document the project...";
+  mainText[11] = "Oh... I thought I was going to be working on the development team?";
+  mainText[12] = "Hm, well, you’re a little new for that, aren’t you? ";
+  mainText[13] = "Don’t want you to end up all confused.";
+  mainText[14] = "Maybe we can revisit that later.";
+  mainText[15] = "The job description said they needed a new developer because they needed help with the project...";
+  mainText[16] = "Oh, okay. I understand...";
+  mainText[17] = "So, you should head on over to Mike now. He’ll show you where to go.";
+  mainText[18] = "Okay! Thank you!";
+  mainText[19] = "Hm, that was a bit disappointing, I was really excited to utilize my programming skills… I have a lot of experience in web dev and I was hoping I could apply them.";
+  mainText[20] = "Well hopefully I’ll get to do that on the next project...";
   // conversation with colleague begins 
-  mainText[4] = "Hey, could you get me a coffee?";
-  mainText[4] = "...what?";
-  mainText[4] = "Listen, I’m super stressed with this big tech project, so some coffee would really hit the spot right now. Okay?";
-  mainText[4] = "Uh... are you Mike?";
-  mainText[4] = "Yeah?";
-  mainText[4] = "Umm... I’m supposed to be helping you with the project…";
-  mainText[4] = "What?";
-  mainText[4] = "I’m the new developer here…";
-  mainText[4] = "Haha, woah, really? I thought you were our new secretary.";
-  mainText[4] = "...I mean, I am just documenting the code…";
-  mainText[4] = "Oh, that seems a little more fit for you.";
-  mainText[4] = "...Can you still grab me that coffee, though?";
-  mainText[4] = "Umm... okay... sure...";
+  mainText[21] = "Hey, could you get me a coffee?";
+  mainText[22] = "...what?";
+  mainText[23] = "Listen, I’m super stressed with this big tech project, so some coffee would really hit the spot right now. Okay?";
+  mainText[24] = "Uh... are you Mike?";
+  mainText[25] = "Yeah?";
+  mainText[26] = "Umm... I’m supposed to be helping you with the project…";
+  mainText[27] = "What?";
+  mainText[28] = "I’m the new developer here…";
+  mainText[29] = "Haha, woah, really? I thought you were our new secretary.";
+  mainText[30] = "...I mean, I am just documenting the code…";
+  mainText[31] = "Oh, that seems a little more fit for you.";
+  mainText[32] = "...Can you still grab me that coffee, though?";
+  mainText[33] = "Umm... okay... sure...";
   // breakdown monologue begins
-  mainText[4] = "I was so excited to work on the project...";
-  mainText[4] = "Maybe I should just forget about all of this...";
-  mainText[4] = "Applying to this job was such a stupid idea anyways, why did I ever think I could become some big-shot developer?";
-  mainText[4] = "I shouldn’t have even bothered trying...";
-  mainText[4] = "I’m really not cut out for this, am I?";
-  mainText[4] = "...What is even the point of this...";
+  mainText[34] = "I was so excited to work on the project...";
+  mainText[35] = "Maybe I should just forget about all of this...";
+  mainText[36] = "Applying to this job was such a stupid idea anyways, why did I ever think I could become some big-shot developer?";
+  mainText[37] = "I shouldn’t have even bothered trying...";
+  mainText[38] = "I’m really not cut out for this, am I?";
+  mainText[39] = "...What is even the point of this...";
 
 }
 
