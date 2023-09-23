c3159899aa98bc3cd724f24a40cdbb712bd0fb2f
Yasmin Motahhary <yasminmotahhary@gmail.com>
2023-09-23 13:12:07 -0400

    Changed canvas size

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(200);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
